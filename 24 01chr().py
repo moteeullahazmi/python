001d4c54fba2fe0ae6c5d4afc29a390662a81326
moteeullahazmi <moteeullahazmi@gmail.com>
2022-06-20 09:20:10 +0530

    commit

---
+++ 24 01chr().py
@@ -0,0 +1,11 @@
+#chr(): take an integer in covert to character c, so it return string
+
+v=65
+print(chr(v))  #ASCII value is ans
+
+print(type(v),chr(v))   #type check is string
+
+
+
+
+#ASCII: ASCII (American Standard Code for Information Interchange) is the most common character encoding format for text data in computers and on the internet.
